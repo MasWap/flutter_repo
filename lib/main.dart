2d742ba4f9faebed12db0082aa8c0fd55eea7ac6
MasWap <lilian.layrac@gmail.com>
2022-12-19 17:16:40 +0100

    Avancement sur le tp 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_repo/models/habitation.dart';
+import 'package:flutter_repo/models/type_habitats.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,92 +26,84 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyHomePage extends StatelessWidget {
   final String title;
+  MyHomePage({required this.title, Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  var _typehabitats = [TypeHabitat(1, "Maison"), TypeHabitat(1, "Appartement")];
+  var _habitations = [
+    Habitaiton(1, "maison.png", "Maison méditerranéenne",
+        "12 Rue du Coq qui chante", 3, 92, 600),
+    Habitaiton(2, "appartement.png", "Appartement neuf", "Rue de la soif",
+        1, 50, 555),
+    Habitaiton(3, "appartement.png", "Appartement 1", "Rue 1", 1, 51, 401),
+    Habitaiton(4, "appartement.png", "Appartement 2", "Rue 2", 2, 52, 402),
+    Habitaiton(5, "maison.png", "Maison 1", "Rue M1", 3, 101, 701),
+    Habitaiton(6, "maison.png", "Maison 2", "Rue M2", 3, 102, 702),
+  ];
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  _buildTypeHabitat() {
+    return Container(
+      height: 100,
+      child: Row(
+        children: List.generate(
+          _typehabitats.length,
+            (index) => _buildHabitat(_typehabitats[index])(),
+        ),
+      ),
+    );
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _buildHabitat(TypeHabitat typeHabitat) {
+    var icon = Icons.house;
+    switch (typeHabitat.id) {
+      // case 1! House
+      case 2:
+        icon = Icons.apartment;
+        break;
+      default:
+        icon = Icons.home;
+    }
+    return Container(
+      height: 80,
+      child: Row(
+        children: [Icon(icon), Text(typeHabitat.libelle)],
+      ),
+    );
+  }
+
+  _buildDerniereLocation(BuildContext context) {
+    return Container(
+      height: 240,
+      child: ListView.builder(
+        itemBuilder: _habitations.length,
+        itemExtent: 220,
+        itemBuilder: (context, index) =>
+            _buildRow(_habitations[index], context),
+        scrollDirection: Axis.horizontal,
+      ),
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build (BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            _buildTypeHabitat(),
+            _buildDerniereLocation(context),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/models/habitation.dart
@@ -0,0 +1,13 @@
+class Habitaiton {
+  int id;
+  String image;
+  String libelle;
+  String adresse;
+  int chambres;
+  int superficie;
+  double prixmois;
+
+  Habitaiton(this.id, this.image, this.libelle, this.adresse, this.chambres,
+      this.superficie, this.prixmois);
+}
+
